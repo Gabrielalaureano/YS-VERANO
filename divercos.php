5682e5bba24f1937349b3686172789d227d3ffc9
Gabrielalaureano <GabrielacostadeFaria@gmail.com>
2023-02-06 18:09:42 -0300

    06-02-2023

--- divercos.php
+++ divercos.php
@@ -4,7 +4,82 @@ require_once 'menu.php';
 
 ?>
 
-<p> TESTE</P>
+<div class="feito text-center">
+    <H1>FEITO PRA VOCÊ</H1>
+
+</div>
+<!--categoria bnt-->
+<button type="button" class="btn" ><i class="fa-solid fa-list"></i>Categorias</button>
+
+<div class="vendidos text-center">
+<p>Mais vendidos</p></div>
+
+<!--cads cangas-->
+<div class="row">
+      <div class="card preto">
+        <img class="image" src="img\canga01.jpg" alt="money" />        
+        <h2>Canga Turquesa.
+        </h2>
+        <p>R$ 95,00</p>
+        <p>Em até 3x de R$66,66 sem juros</p>
+        <a  href="#" class="btn btn-dark">Adicionar ao Carrinho</a>
+      </div>
+ 
+      <div class="card vermelho">
+     
+        <img class="image ver" src="img\canga02.jpg" alt="settings" />
+        <h2>Canga Pink Baby.
+        </h2>
+        <p>R$150,00</p>
+        <p>Em até 3x de R$66,66 sem juros</p>
+        <a  href="#" class="btn btn-dark">Adicionar ao Carrinho</a>
+      </div>
+ 
+     <div class="card terceiro">
+        <img class="image" src="img\canga03.jpg" alt="article" />
+        <h2>Canga Tropical.
+        </h2>
+        <p>R$ 250,00</p>
+        <p>Em até 3x de R$66,66 sem juros</p>
+        <a  href="#" class="btn btn-dark">Adicionar ao Carrinho</a>
+      </div>
+      </div>
+      <p>bronzeadores</p></div>
+
+
+      <!--cads biquinis-->
+      <div class="row">
+      <div class="card preto">
+        <img class="image" src="img\canga01.jpg" alt="money" />        
+        <h2>Canga Turquesa.
+        </h2>
+        <p>R$ 95,00</p>
+        <p>Em até 3x de R$66,66 sem juros</p>
+        <a  href="#" class="btn btn-dark">Adicionar ao Carrinho</a>
+      </div>
+ 
+      <div class="card vermelho">
+     
+        <img class="image ver" src="img\canga02.jpg" alt="settings" />
+        <h2>Canga Pink Baby.
+        </h2>
+        <p>R$150,00</p>
+        <p>Em até 3x de R$66,66 sem juros</p>
+        <a  href="#" class="btn btn-dark">Adicionar ao Carrinho</a>
+      </div>
+ 
+     <div class="card terceiro">
+        <img class="image" src="img\canga03.jpg" alt="article" />
+        <h2>Canga Tropical.
+        </h2>
+        <p>R$ 250,00</p>
+        <p>Em até 3x de R$66,66 sem juros</p>
+        <a  href="#" class="btn btn-dark">Adicionar ao Carrinho</a>
+      </div>
+      </div>
+      <p>bronzeadores</p></div>
+
+  <!--fechamento Bronzeador-->
 
 
 <?php
